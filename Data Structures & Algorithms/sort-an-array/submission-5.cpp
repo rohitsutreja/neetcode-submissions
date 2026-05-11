b69546bdf4cde41f276ba12753b6010173097bc4
rohitsutreja <128723626+rohitsutreja@users.noreply.github.com>
2026-05-11 16:57:44 +0530

    Add: sort-an-array - submission-5

---
+++ Data Structures & Algorithms/sort-an-array/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quick(nums, 0, nums.size() - 1);
+        return nums;
+    }
+
+    int partition(vector<int>& nums, int start, int end){
+        int pivot = nums[end];
+
+        int write = start;
+        
+        for(int read = start; read <= end; ++read){
+            if(nums[read] <= pivot){
+                std::swap(nums[write], nums[read]);
+                write++;
+            }
+        }   
+
+        return write - 1;
+    }
+
+
+    void quick(vector<int>& nums, int start,int end){
+        if (start >= end) return;
+
+        int mid = partition(nums, start, end);
+        quick(nums, start, mid - 1);
+        quick(nums, mid + 1, end);
+    }
+};
