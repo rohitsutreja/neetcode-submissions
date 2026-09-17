633c946df1d38e9bee194aec90401828e4d06540
rohitsutreja <128723626+rohitsutreja@users.noreply.github.com>
2026-09-17 16:57:38 +0530

    Add: binary-tree-postorder-traversal - submission-6

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-6.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    vector<int> iterative(TreeNode* root) {
+        if(!root){
+            return {};
+        }
+        vector<int> result{};
+
+        stack<std::pair<TreeNode*, bool>> s{};
+        s.push({root, false});
+
+        while (!s.empty()) {
+            auto [cur, v] = s.top();
+
+            if (v) {
+                s.pop();
+                result.push_back(cur->val);
+            } else {
+                s.top().second = true;
+                if (cur->right) {
+                    s.push({cur->right, false});
+                }
+                if(cur->left){
+                    s.push({cur->left, false});
+                }
+            }
+        }
+
+        return result;
+    }
+
+    vector<int> recursive(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+
+        auto l = postorderTraversal(root->left);
+        auto r = postorderTraversal(root->right);
+
+        l.insert(l.end(), r.begin(), r.end());
+        l.push_back(root->val);
+
+        return l;
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) { return iterative(root); }
+};
